0c89402c0a066c86419716223dfa7a4072851b80
Daniel Parker <danielparker134@gmail.com>
2020-12-01 15:26:57 -0500

    Temporary Sprites and Scenes Added

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Object to Lock on")]
+    public GameObject player;
+
+    [Header("Camera Offset")]
+    public Vector3 offset = new Vector3(-4.5f, 0, 10);
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(player.transform.position.x + offset.x, offset.y, offset.z);
+    }
+}
