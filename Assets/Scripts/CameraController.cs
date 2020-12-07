8c0198bda576dde43957a2335c316162d409d965
Daniel Parker <danielparker134@gmail.com>
2020-12-07 01:08:31 -0500

    Added pickup itens
    - Fixed some camera and movement issues
    - Modified player and pipe sizes to help with physics

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,10 +9,13 @@ public class CameraController : MonoBehaviour
 
     [Header("Camera Controls")]
     public Vector3 offset = new Vector3(-4.5f, 0, 10);
+    public float moveSpeed = 0.5f;
 
-    void Update()
+    void LateUpdate()
     {
+        Vector3 cameraPos = new Vector3(player.transform.position.x + offset.x, offset.y, offset.z);
+
         //Tracks the player with the added offset to see forward more
-        this.transform.position = new Vector3(player.transform.position.x + offset.x, offset.y, offset.z);
+        transform.position = Vector3.Lerp(transform.position, cameraPos, moveSpeed * Time.deltaTime);
     }
 }
--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -6,11 +6,16 @@ public class MapGenerator : MonoBehaviour
 {
     [Header("Prefabs")]
     public GameObject pipePrefab;
+    public GameObject pickupPrefab;
 
-    [Header("Spawning Settings")]
+    [Header("Pipe Spawning Settings")]
     public int numPipes = 6;
     public float spacing = 4f;
     public float randHeightOffset = 1.5f;
+
+    [Header("Pickup Spawning Settings")]
+    public float randPickupSpawnChance = 0.35f;
+    public Vector2 randPosOffset = new Vector2(0.5f, 1f);
 
     private float currentSpawnOffset = 0;
 
@@ -19,26 +24,36 @@ public class MapGenerator : MonoBehaviour
     {
         for (int i = 0; i < numPipes; i++)
         {
-            float randHeight = 0;
-
-            if (i != 0)
-                randHeight = Random.Range(-randHeightOffset, randHeightOffset);
-
-            GameObject currentPipe = Instantiate<GameObject>(pipePrefab);
-            currentPipe.transform.position = new Vector3(currentPipe.transform.position.x + currentSpawnOffset, randHeight, currentPipe.transform.position.z);
-            currentPipe.transform.parent = this.transform;
-
-            currentSpawnOffset += spacing;
+            if (i == 0)
+                SpawnPipeNotRandom();
+            else
+                SpawnPipe();
         }
+    }
+
+    public void SpawnPipeNotRandom()
+    {
+        GameObject currentPipe = Instantiate<GameObject>(pipePrefab, this.transform);
+        currentPipe.transform.position = new Vector3(currentPipe.transform.position.x + currentSpawnOffset, 0, currentPipe.transform.position.z);
+
+        currentSpawnOffset += spacing;
     }
 
     public void SpawnPipe()
     {
         float randHeight = Random.Range(-randHeightOffset, randHeightOffset);
 
-        GameObject currentPipe = Instantiate<GameObject>(pipePrefab);
+        GameObject currentPipe = Instantiate<GameObject>(pipePrefab, this.transform);
         currentPipe.transform.position = new Vector3(currentPipe.transform.position.x + currentSpawnOffset, randHeight, currentPipe.transform.position.z);
-        currentPipe.transform.parent = this.transform;
+
+        if(Random.value <= randPickupSpawnChance)
+        {
+            randHeight = Random.Range(-randPosOffset.y, randPosOffset.y);
+            float randDist = Random.Range(-randPosOffset.x, randPosOffset.y);
+
+            GameObject pickupObject = Instantiate(pickupPrefab);
+            pickupObject.transform.position = new Vector3((currentPipe.transform.position.x + (spacing / 2f)) + randDist, randHeight, currentPipe.transform.position.z);
+        }
 
         currentSpawnOffset += spacing;
     }
